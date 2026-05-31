84d1b24c8cc62a7dfcae79bf7cbcf8ff1ee28df7
nishmishra <nishmishra@gmail.com>
2026-05-31 13:23:27 -0400

    Add files via upload

---
+++ ivypandit-final-production-grade/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import { ContactSection } from "../../components/Site";
+
+export const metadata = { title: "Contact | Ivy Pandit" };
+
+export default function ContactPage() {
+  return <main className="pageTop"><ContactSection /></main>;
+}
