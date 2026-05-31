c9c736b719bcff771010ffa9abf9eaafa07b9d5e
nishmishra <nishmishra@gmail.com>
2026-05-31 16:43:27 -0400

    Add files via upload

---
+++ app/hi/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+  return (
+    <main style={{padding:"60px"}}>
+      <h1>आइवी पण्डित</h1>
+      <p>आधुनिक तंत्रिका-विज्ञान, संस्कृत और सनातन धर्म के मध्य संवाद।</p>
+      <p><a href="/">English</a></p>
+    </main>
+  );
+}
---
+++ app/sa/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+  return (
+    <main style={{padding:"60px"}}>
+      <h1>आइवी पण्डितः</h1>
+      <p>आधुनिक-तन्त्रिका-विज्ञानस्य संस्कृत-साहित्यस्य च संवादः।</p>
+      <p><a href="/">English</a></p>
+    </main>
+  );
+}
