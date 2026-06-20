be11cfe12d7e2b5d6188267d84f5ebf652d3f55e
nishmishra <nishmishra@gmail.com>
2026-06-20 01:04:44 -0400

    Delete ivypandit-final-production-grade directory

--- ivypandit-final-production-grade/app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ContactSection } from "../../components/Site";
-
-export const metadata = { title: "Contact | Ivy Pandit" };
-
-export default function ContactPage() {
-  return <main className="pageTop"><ContactSection /></main>;
-}
--- ivypandit-final-production-grade/app/research/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ResearchSection, TopicsSection } from "../../components/Site";
-
-export const metadata = { title: "Research Hub | Ivy Pandit" };
-
-export default function ResearchPage() {
-  return <main className="pageTop"><ResearchSection /><TopicsSection /></main>;
-}
