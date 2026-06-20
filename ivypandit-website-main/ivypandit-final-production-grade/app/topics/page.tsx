a89f7e8f6beb9930924eabf84c23c5826f949072
nishmishra <nishmishra@gmail.com>
2026-06-20 01:05:07 -0400

    Delete ivypandit-website-main directory

--- ivypandit-website-main/ivypandit-final-production-grade/app/topics/page.tsx
+++
@@ -1,7 +0,0 @@
-import { TopicsSection, ResearchSection } from "../../components/Site";
-
-export const metadata = { title: "Topics | Ivy Pandit" };
-
-export default function TopicsPage() {
-  return <main className="pageTop"><TopicsSection /><ResearchSection /></main>;
-}
--- ivypandit-website-main/ivypandit-final-production-grade/app/videos/page.tsx
+++
@@ -1,7 +0,0 @@
-import { VideosSection, BannerStrip, ContactSection } from "../../components/Site";
-
-export const metadata = { title: "Videos | Ivy Pandit" };
-
-export default function VideosPage() {
-  return <main className="pageTop"><BannerStrip /><VideosSection /><ContactSection /></main>;
-}
